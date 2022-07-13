07dfe589fa6ed5e5ffb3ffbcdff321e2e09fd9df
Mikebainbain <mlopez749@gmail.com>
2022-07-12 20:09:18 -0500

    makemigration inconcluso

--- responsivas/apps/app1/models.py
+++ responsivas/apps/app1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    id = models.AutoField(Primary_key = True)
+    id = models.AutoField(primary_key = True)
     nombres = models.CharField(max_leght = 40, blank = False, null = False)
     apellidoP = models.CharField(max_leght = 30, blank = False, null = False)
     apellidoM = models.CharField(max_legth= 30, blank = False, null = False)
